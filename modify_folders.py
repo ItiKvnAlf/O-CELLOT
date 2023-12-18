21cac5268ef08255aff20d0ce310d1f67385797d
Matias Galli <matias.galli@alumnos.ucn.cl>
2023-12-17 22:15:28 -0300

    Refactor file and folder renaming logic

--- modify_folders.py
+++ modify_folders.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 
 load_dotenv() # Cargar variables de entorno
 
+
 current_folder = os.getcwd()  # Carpeta actual
 
 init_time = time.time() #Variable para calcular tiempo de ejecución
@@ -16,22 +17,23 @@ def rename_files(root_folder, type):
 
     for foldername, subfolders, filenames in os.walk(root_folder): # Recorre la carpeta de boletas por cada carpeta y archivo
         for filename in filenames:
-            anio = filename.split('-')[-1].split('.')[0]
-            mes = filename.split('-')[-2]
-            dia = (''.join(filter(str.isdigit, filename.split('-')[0]))).zfill(2)
+            if filename != '.DS_Store':
+                anio = filename.split('-')[-1].split('.')[0]
+                mes = filename.split('-')[-2]
+                dia = (''.join(filter(str.isdigit, filename.split('-')[0]))).zfill(2)
 
-            # Modificar nombres de archivos dentro de meses
-            if mes.lower() in meses: # Si el nombre del mes está en la lista de meses
-                mes = str(meses.index(mes.lower()) + 1).zfill(2)
-            
-            if filename.endswith('.csv'):  # Si el archivo es un csv
-                # Generar el nuevo nombre del archivo
-                new_filename = f'{type}{anio}-{mes}-{dia}.csv'
+                # Modificar nombres de archivos dentro de meses
+                if mes.lower() in meses: # Si el nombre del mes está en la lista de meses
+                    mes = str(meses.index(mes.lower()) + 1).zfill(2)
+                
+                if filename.endswith('.csv'):  # Si el archivo es un csv
+                    # Generar el nuevo nombre del archivo
+                    new_filename = f'{type}{anio}-{mes}-{dia}.csv'
 
-                file_path = os.path.join(foldername, filename)  # Ruta del archivo
-                new_file_path = os.path.join(foldername, new_filename)  # Ruta del nuevo archivo
+                    file_path = os.path.join(foldername, filename)  # Ruta del archivo
+                    new_file_path = os.path.join(foldername, new_filename)  # Ruta del nuevo archivo
 
-                os.rename(file_path, new_file_path)  # Renombrar el archivo
+                    os.rename(file_path, new_file_path)  # Renombrar el archivo
 
 def rename_folders(root_folder):
 
@@ -39,10 +41,13 @@ def rename_folders(root_folder):
              'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
 
     for foldername, subfolders, filenames in os.walk(root_folder): # Recorre la carpeta de boletas por cada carpeta y archivo
-        if foldername.split('\\')[-1].lower() in meses:
-                anio = foldername.split('\\')[-2]
-                mes = foldername.split('\\')[-1]
 
+        if foldername.split('/')[-1].lower() in meses:
+
+                anio = foldername.split('/')[-2]
+
+                mes = foldername.split('/')[-1]
+                print(mes,anio)
                 if mes.lower() in meses:
                     mes = str(meses.index(mes.lower()) + 1).zfill(2)
                 
@@ -54,8 +59,8 @@ def rename_folders(root_folder):
 
     execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
 
-rename_files(os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), "Boletas"), "boletas") # Renombrar carpetas y archivos en Boletas
-rename_files(os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), "Facturas"), "facturas") # Renombrar carpetas y archivos en Facturas
+#rename_files(os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), "Boletas"), "boletas") # Renombrar carpetas y archivos en Boletas
+#rename_files(os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), "Facturas"), "facturas") # Renombrar carpetas y archivos en Facturas
 
 rename_folders(os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), "Boletas")) # Renombrar carpetas y archivos en Boletas
 rename_folders(os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), "Facturas")) # Renombrar carpetas y archivos en Facturas
@@ -64,3 +69,4 @@ rename_folders(os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), "Pre
 execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
 print('\n-\tTiempo de ejecución:', execution_time, 'segundos') # Imprime el tiempo de ejecución
 
+# Funcional en Windows y MacOS
