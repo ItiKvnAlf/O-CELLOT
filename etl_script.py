92e7592e9f1291a743515f3645f2d4a2a75a0c51
Matias Galli <matias.galli@alumnos.ucn.cl>
2023-12-18 11:13:53 -0300

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better performance
    
    Update README with installation instructions
    
    Fix formatting issues in CSS
    
    Add error handling for database connection
    
    Update API endpoint for better security
    
    Remove unused variables
    
    Fix broken links in navigation
    
    Add unit tests for new features

--- etl_script.py
+++ etl_script.py
@@ -2,8 +2,8 @@ import os
 import time
 import pandas as pd
 from dotenv import load_dotenv
-import calendar
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
+
 
 load_dotenv() # Cargar variables de entorno
 
@@ -42,53 +42,53 @@ def extract_prices():
 
 def extract_vouchers():
     
-    init_time = time.time() #Variable para calcular tiempo de ejecución
+    init_time = time.time()
      
-    vouchers_root_folder = os.path.join(current_folder, '..', os.getenv('DATA_FOLDER') , 'Boletas')  # Carpeta de boletas
-    vouchers_dataframes = [] # Lista de dataframes de boletas
+    vouchers_root_folder = os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), 'Boletas')
+    vouchers_dataframes = []
     
-    for foldername, subfolders, filenames in os.walk(vouchers_root_folder): # Recorre la carpeta de boletas por cada carpeta y archivo
-        for filename in filenames:  # Recorre los archivos de cada carpeta
-            anio = filename.split('-')[-1].split('.')[0]
-            mes = filename.split('-')[-2]
-            dia = ''.join(filter(str.isdigit, filename.split('-')[0]))
-            if filename.endswith('.csv'):   # Si el archivo es un csv
-                file_path = os.path.join(foldername, filename)  # Ruta del archivo
+    for foldername, subfolders, filenames in os.walk(vouchers_root_folder):
+        for filename in filenames:
+            if filename != '.DS_Store' and filename.endswith('.csv'):  # Check for file extension
+                file_path = os.path.join(foldername, filename)
+                
+                # Parse file name to get anio, mes, and dia
+                anio = filename.split('-')[-1].split('.')[0]
+                mes = filename.split('-')[-2]
+                dia = ''.join(filter(str.isdigit, filename.split('-')[0]))
+                
                 rows = []
                 with open(file_path, 'r') as file:
                     next(file)
                     for line in file:
                         line = line.strip()
                         parts = line.split(',')
-                        
-                        # Los campos intermedios son productos, y se unen nuevamente en un solo campo
                         products = parts[1:-1]
-                        
                         rows.append([parts[0], products, parts[-1], anio, mes, dia])
+        
+                df = pd.DataFrame(rows, columns=['Boleta', 'Productos', 'Precio Total', 'Anio', 'Mes', 'Dia'])
+                vouchers_dataframes.append(df)
     
-                df = pd.DataFrame(rows, columns=['Boleta', 'Productos', 'Precio Total', 'Anio', 'Mes', 'Dia']) # Crear un DataFrame de pandas
+    execution_time = round(time.time() - init_time, 3)
     
-                vouchers_dataframes.append(df)  # Agrega el dataframe a la lista
-    
-    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
-    
-    print('\n-\tTiempo de ejecución carpeta Boletas:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    print('\n-\tTiempo de ejecución carpeta Boletas:', execution_time, 'segundos')
     return vouchers_dataframes
 
 def extract_bills():
     
-    init_time = time.time() #Variable para calcular tiempo de ejecución
+    init_time = time.time()
     
-    bills_root_folder = os.path.join(current_folder, '..',os.getenv('DATA_FOLDER') , 'Facturas')  # Carpeta de facturas
-    bills_dataframes = [] # Lista de dataframes de facturas
+    bills_root_folder = os.path.join(current_folder, '..', os.getenv('DATA_FOLDER'), 'Facturas')
+    bills_dataframes = []
     
-    for foldername, subfolders, filenames in os.walk(bills_root_folder): # Recorre la carpeta de facturas por cada carpeta y archivo
-        for filename in filenames:  # Recorre los archivos de cada carpeta
-            anio = filename.split('-')[-1].split('.')[0]
-            mes = filename.split('-')[-2]
-            dia = (''.join(filter(str.isdigit, filename.split('-')[0])))
-            if filename.endswith('.csv'):   # Si el archivo es un csv
-                file_path = os.path.join(foldername, filename)  # Ruta del archivo
+    for foldername, subfolders, filenames in os.walk(bills_root_folder):
+        for filename in filenames:
+            if filename != '.DS_Store' and filename.endswith('.csv'):
+                file_path = os.path.join(foldername, filename)
+                anio = filename.split('-')[-1].split('.')[0]
+                mes = filename.split('-')[-2]
+                dia = ''.join(filter(str.isdigit, filename.split('-')[0]))
+                
                 rows = []
                 with open(file_path, 'r') as file:
                     next(file)
@@ -96,15 +96,14 @@ def extract_bills():
                         line = line.strip()
                         parts = line.split(',')
                         
-                        rows.append([parts[0], parts[1], parts[2],parts[3],parts[4],anio,mes,dia])
+                        rows.append([parts[0], parts[1], parts[2], parts[3], parts[4], anio, mes, dia])
     
-                df = pd.DataFrame(rows, columns=['Proveedor', 'Producto', 'Cantidad', 'Precio Unitario', 'Precio Total', 'Anio', 'Mes', 'Dia']) # Crear un DataFrame de pandas
-        
-                bills_dataframes.append(df)  # Agrega el dataframe a la lista
-
-    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
+                df = pd.DataFrame(rows, columns=['Proveedor', 'Producto', 'Cantidad', 'Precio Unitario', 'Precio Total', 'Anio', 'Mes', 'Dia'])
+                bills_dataframes.append(df)
     
-    print('\n-\tTiempo de ejecución carpeta Facturas:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    execution_time = round(time.time() - init_time, 3)
+    
+    print('\n-\tTiempo de ejecución carpeta Facturas:', execution_time, 'segundos')
     return bills_dataframes
 
 def read_products():
@@ -118,90 +117,78 @@ def read_products():
                 products.append(df.iloc[i]['ID'])
     return products
 
-def transform_vouchers(vouchers: list, bills: list, products: list):
-    products_amount = len(products)
-    amounts = [100] * products_amount
-    inventory = []
-    product_not_found = []
-    cant = 0
+def transform_to_inventory(vouchers: list, bills: list, products: list):
+
+    init_time = time.time() #Variable para calcular tiempo de ejecución
+
+    products_amount = len(products) # Cantidad de productos
+    amounts = [100] * products_amount # Cantidad de productos en el inventario (100 unidades de cada producto)
+    inventory = [] # Lista de dataframes de inventario
+    product_not_found = [] # Lista de productos no encontrados
     for i in range(len(bills)):
-        rows = []
-        bill = bills[i]
-        voucher = vouchers[i]
-        print(cant)
-        for j in range(len(bill)):
-            product = bill.iloc[j]['Producto']
+        rows = [] # Lista de filas del dataframe
+        bill = bills[i] # Factura
+        voucher = vouchers[i] # Boleta
+        for j in range(len(bill)): # Recorre la factura
+            product = bill.iloc[j]['Producto'] # Producto
             if product not in products:
                 if product not in product_not_found:
-                        product_not_found.append(product)
+                        product_not_found.append(product) # Agrega el producto a la lista de productos no encontrados
             else:
-                product_id = products.index(product)
-                amounts[product_id] += int(bill.iloc[j]['Cantidad'])
-        for k in range(len(voucher)):
-            product_list = voucher.iloc[k]['Productos']
-            for product in product_list:
+                product_id = products.index(product) # ID del producto
+                amounts[product_id] += int(bill.iloc[j]['Cantidad']) # Aumenta la cantidad de productos en el inventario
+        for k in range(len(voucher)): # Recorre la boleta
+            product_list = voucher.iloc[k]['Productos'] # Lista de productos
+            for product in product_list: 
                 if product not in products:
                     if product not in product_not_found:
-                        product_not_found.append(product)
+                        product_not_found.append(product) # Agrega el producto a la lista de productos no encontrados
                 else:
-                    product_id = products.index(product)
-                    amounts[product_id] -= 1
+                    product_id = products.index(product) # ID del producto
+                    amounts[product_id] -= 1 # Disminuye la cantidad de productos en el inventario
         for l in range(len(products)):
-            rows.append([products[l], amounts[l], voucher.iloc[i]['Anio'], voucher.iloc[i]['Mes'], voucher.iloc[i]['Dia']])
-        df = pd.DataFrame(rows, columns=['Producto','Cantidad','Anio','Mes','Dia'])
-        inventory.append(df)
-        cant += 1
+            rows.append([products[l], amounts[l], voucher.iloc[0]['Anio'], voucher.iloc[0]['Mes'], voucher.iloc[0]['Dia']]) # Agrega la fila al dataframe
+        df = pd.DataFrame(rows, columns=['Producto','Cantidad','Anio','Mes','Dia']) # Crear un DataFrame de pandas
+        inventory.append(df)    # Agrega el dataframe a la lista
     print('\nALERTA | Algunos productos no están registrados:')
     for product in product_not_found:
-        print("\n\t-",product)
-    print("\n")
+        print("\n\t-",product) # Imprime los productos no encontrados
+    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
+    
+    print('\n-\tTiempo de ejecución carpeta Facturas:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    print('-\tCantidad de productos no encontrados:', len(product_not_found)) # Imprime la cantidad de productos no encontrados
+    print('-\tCantidad de archivos Inventario:', len(inventory)) #Imprime la cantidad de inventario
+    # print('\n',inventory[0].head())
     return inventory
 
+def dataframe_to_sql(dataframe: list, table_name: str, engine: Engine):
+    init_time = time.time() #Variable para calcular tiempo de ejecución
+    for i, df in enumerate(dataframe): # Recorre la lista de dataframes de precios
+        df.to_sql(table_name, con=engine, if_exists='append', index=False) # Inserta los datos del dataframe en la tabla
+    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
+
+    print('\n-\tSe han insertado los datos de', table_name, 'en la base de datos')
+    print('-\tTiempo de ejecución:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+
+print('\nIniciando script de ETL')
+print('-----------------------')
+print('\nLeyendo archivos de datos...')
 products = read_products()
 
 prices = extract_prices()
-print('-\tCantidad de archivos Precios:', len(prices)) #Imprime la cantidad de precios
-print('\n',prices[0].head())
-
 vouchers = extract_vouchers()
-print('-\tCantidad de archivos Boletas:', len(vouchers)) #Imprime la cantidad de boletas
-print('\n',vouchers[0].head())
-
 bills = extract_bills()
-print('-\tCantidad de archivos Facturas:', len(bills)) #Imprime la cantidad de facturas
-print('\n',bills[0].head())
 
-inventory = transform_vouchers(vouchers, bills, products)
+print('\nTransformando datos...')
+inventory = transform_to_inventory(vouchers, bills, products)
 
-for i, df in enumerate(inventory):
-    print(df)
-print('-\tCantidad de archivos Inventario:', len(inventory)) #Imprime la cantidad de inventario
-
+print('\nInsertando datos en la base de datos POSTGRESQL...')
 engine = create_engine(os.getenv('DATABASE_URL')) # DATABASE_URL es una variable de entorno que contiene la cadena de conexión a la base de datos
 
-for i, df in enumerate(prices):
-    table_name = 'price'
-    df.to_sql(table_name, con=engine, if_exists='append', index=False)
-
-print('\nSe han insertado los datos de precios')
-
-for i, df in enumerate(vouchers):
-    table_name = 'voucher'
-    df.to_sql(table_name, con=engine, if_exists='append', index=False)
-
-print('\nSe han insertado los datos de boletas')
-
-for i, df in enumerate(bills):
-    table_name = 'bill'
-    df.to_sql(table_name, con=engine, if_exists='append', index=False)
-
-print('\nSe han insertado los datos de facturas')
-
-for i, df in enumerate(inventory):
-    table_name = 'inventory'
-    df.to_sql(table_name, con=engine, if_exists='append', index=False)
-
-print('\nSe han insertado los datos de inventario')
+dataframe_to_sql(prices, 'precios', engine)
+dataframe_to_sql(vouchers, 'boletas', engine)
+dataframe_to_sql(bills, 'facturas', engine)
+dataframe_to_sql(inventory, 'inventario', engine)
 
 engine.dispose() # Cerrar conexión a la base de datos
-print('\n\tSe ha cerrado la conexión a la base de datos')
+print('\nSe ha cerrado la conexión a la base de datos')
