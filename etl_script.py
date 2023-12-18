f93513959fbd166fc6f98321c6eb6d45be81e343
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-12-18 02:40:33 -0300

    Refactor code and add print statements for execution time and data count

--- etl_script.py
+++ etl_script.py
@@ -2,7 +2,7 @@ import os
 import time
 import pandas as pd
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 
 load_dotenv() # Cargar variables de entorno
 
@@ -37,6 +37,8 @@ def extract_prices():
     execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
     
     print('\n-\tTiempo de ejecución carpeta Precio:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    print('\n-\tCantidad de archivos Precios:', len(prices_dataframes)) #Imprime la cantidad de precios
+    # print('\n',prices_dataframes[0].head())
     return prices_dataframes
 
 def extract_vouchers():
@@ -72,6 +74,8 @@ def extract_vouchers():
     execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
     
     print('\n-\tTiempo de ejecución carpeta Boletas:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    print('\n-\tCantidad de archivos Boletas:', len(vouchers_dataframes)) #Imprime la cantidad de boletas
+    # print('\n',vouchers_dataframes[0].head())
     return vouchers_dataframes
 
 def extract_bills():
@@ -104,9 +108,12 @@ def extract_bills():
     execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
     
     print('\n-\tTiempo de ejecución carpeta Facturas:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    print('\n-\tCantidad de archivos Facturas:', len(bills_dataframes)) #Imprime la cantidad de facturas
+    # print('\n',bills_dataframes[0].head())
     return bills_dataframes
 
 def read_products():
+    init_time = time.time() #Variable para calcular tiempo de ejecución
     folder = os.path.join(current_folder, '..', os.getenv('DATA_FOLDER') , 'Productos')  # Carpeta de productos
     products = [] # Lista de productos
     for filename in os.listdir(folder):  # Recorre los archivos de la carpeta
@@ -115,9 +122,14 @@ def read_products():
             df = pd.read_excel(file_path) # Leer el archivo
             for i in range(len(df)): # Recorre el archivo
                 products.append(df.iloc[i]['ID']) # Agrega el producto a la lista
+    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
+    print('\n-\tTiempo de lectura de la carpeta de Productos:', execution_time, 'segundos') # Imprime el tiempo de ejecución
     return products
 
 def transform_vouchers(vouchers: list, bills: list, products: list):
+
+    init_time = time.time() #Variable para calcular tiempo de ejecución
+
     products_amount = len(products) # Cantidad de productos
     amounts = [100] * products_amount # Cantidad de productos en el inventario (100 unidades de cada producto)
     inventory = [] # Lista de dataframes de inventario
@@ -150,52 +162,36 @@ def transform_vouchers(vouchers: list, bills: list, products: list):
     print('\nALERTA | Algunos productos no están registrados:')
     for product in product_not_found:
         print("\n\t-",product) # Imprime los productos no encontrados
-    print("\n")
+    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
+    
+    print('\n-\tTiempo de ejecución carpeta Facturas:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    print('\n-\tCantidad de productos no encontrados:', len(product_not_found)) # Imprime la cantidad de productos no encontrados
+    print('\n-\tCantidad de archivos Inventario:', len(inventory)) #Imprime la cantidad de inventario
+    # print('\n',inventory[0].head())
     return inventory
+
+def dataframe_to_sql(dataframe: list, table_name: str, engine: Engine):
+    init_time = time.time() #Variable para calcular tiempo de ejecución
+    for df in enumerate(dataframe): # Recorre la lista de dataframes de precios
+        df.to_sql(table_name, con=engine, if_exists='append', index=False) # Inserta los datos del dataframe en la tabla
+    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
+
+    print('\n-\tSe han insertado los datos de', table_name, 'en la base de datos')
+    print('\n-\tTiempo de ejecución:', execution_time, 'segundos') # Imprime el tiempo de ejecución
 
 products = read_products()
 
 prices = extract_prices()
-print('-\tCantidad de archivos Precios:', len(prices)) #Imprime la cantidad de precios
-# print('\n',prices[0].head())
-
 vouchers = extract_vouchers()
-print('-\tCantidad de archivos Boletas:', len(vouchers)) #Imprime la cantidad de boletas
-# print('\n',vouchers[0].head())
-
 bills = extract_bills()
-print('-\tCantidad de archivos Facturas:', len(bills)) #Imprime la cantidad de facturas
-# print('\n',bills[0].head())
-
 inventory = transform_vouchers(vouchers, bills, products)
-print('-\tCantidad de archivos Inventario:', len(inventory)) #Imprime la cantidad de inventario
-# print('\n',inventory[0].head())
 
 engine = create_engine(os.getenv('DATABASE_URL')) # DATABASE_URL es una variable de entorno que contiene la cadena de conexión a la base de datos
 
-for i, df in enumerate(prices): # Recorre la lista de dataframes de precios
-    table_name = 'price' # Nombre de la tabla
-    df.to_sql(table_name, con=engine, if_exists='append', index=False) # Inserta los datos del dataframe en la tabla
-
-print('\nSe han insertado los datos de precios')
-
-for i, df in enumerate(vouchers): # Recorre la lista de dataframes de boletas
-    table_name = 'voucher' # Nombre de la tabla
-    df.to_sql(table_name, con=engine, if_exists='append', index=False) # Inserta los datos del dataframe en la tabla
-
-print('\nSe han insertado los datos de boletas')
-
-for i, df in enumerate(bills): # Recorre la lista de dataframes de facturas
-    table_name = 'bill' # Nombre de la tabla
-    df.to_sql(table_name, con=engine, if_exists='append', index=False) # Inserta los datos del dataframe en la tabla
-
-print('\nSe han insertado los datos de facturas')
-
-for i, df in enumerate(inventory): # Recorre la lista de dataframes de inventario
-    table_name = 'inventory' # Nombre de la tabla
-    df.to_sql(table_name, con=engine, if_exists='append', index=False) # Inserta los datos del dataframe en la tabla
-
-print('\nSe han insertado los datos de inventario')
+dataframe_to_sql(prices, 'precios', engine)
+dataframe_to_sql(vouchers, 'boletas', engine)
+dataframe_to_sql(bills, 'facturas', engine)
+dataframe_to_sql(inventory, 'inventario', engine)
 
 engine.dispose() # Cerrar conexión a la base de datos
 print('\n\tSe ha cerrado la conexión a la base de datos')
