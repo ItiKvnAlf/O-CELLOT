d949497803f9059caead60cf1bbc8cedc8e44ad1
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-12-21 19:49:44 -0300

    Add bar chart generation and update home route

--- webpage/app.py
+++ webpage/app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template
+import plotly.express as px
 
 app = Flask(__name__)
 
+# Datos de ejemplo
+ventas_data = [
+    {'Fecha': '2023-01-01', 'Producto': 'A', 'Ventas': 100},
+    {'Fecha': '2023-01-02', 'Producto': 'B', 'Ventas': 150},
+    {'Fecha': '2023-01-03', 'Producto': 'A', 'Ventas': 80},
+]
+
+# Función para generar gráfico de barras
+def generar_grafico_barras():
+    fig = px.bar(ventas_data, x='Fecha', y='Ventas', color='Producto', title='Ventas por Producto')
+    return fig.to_html(full_html=False)
+
+# Función para generar otro gráfico de barras
+def generar_otro_grafico_barras():
+    fig = px.bar(ventas_data, x='Fecha', y='Ventas', color='Producto', barmode='group', title='Otro Gráfico de Barras')
+    return fig.to_html(full_html=False)
+
+# Rutas
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    grafico1 = generar_grafico_barras()
+    grafico2 = generar_otro_grafico_barras()
+    return render_template('home.html', grafico1=grafico1, grafico2=grafico2)
 
 if __name__ == '__main__':
     app.run(debug=True)
