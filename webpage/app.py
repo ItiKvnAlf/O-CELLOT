9b674388f44408b47fbc873a405f8e1b144fb96b
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-12-21 22:43:09 -0300

    Add new functionality and improve code structure

--- webpage/app.py
+++ webpage/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 import plotly.express as px
+import pandas as pd
+import os
+import time
+
 
 app = Flask(__name__)
 
@@ -20,6 +24,20 @@ def generar_otro_grafico_barras():
     fig = px.bar(ventas_data, x='Fecha', y='Ventas', color='Producto', barmode='group', title='Otro Gráfico de Barras')
     return fig.to_html(full_html=False)
 
+def read_products():
+    init_time = time.time() #Variable para calcular tiempo de ejecución
+    folder = os.path.join(os.getcwd(), '..', 'F' , 'Productos')  # Carpeta de productos
+    products = [] # Lista de productos
+    for filename in os.listdir(folder):  # Recorre los archivos de la carpeta
+        if filename.endswith('.xlsx'):  # Si el archivo es un xlsx
+            file_path = os.path.join(folder, filename) # Ruta del archivo
+            df = pd.read_excel(file_path) # Leer el archivo
+            for i in range(len(df)): # Recorre el archivo
+                products.append(df.iloc[i]['ID']) # Agrega el producto a la lista
+    execution_time = round(time.time() - init_time, 3) # Calcular el tiempo de ejecución
+    print('\n-\tTiempo de lectura de la carpeta de Productos:', execution_time, 'segundos') # Imprime el tiempo de ejecución
+    return products
+
 # Rutas
 @app.route('/')
 def home():
@@ -29,6 +47,7 @@ def home():
 
 @app.route('/products')
 def products():
+
     return render_template('products.html')
 
 @app.route('/inventory')
--- webpage/config/dbConnection.py
+++ webpage/config/dbConnection.py
@@ -1,26 +1,32 @@
 import pyodbc
 
-try:
-    connection = pyodbc.connect(
-        'Driver={SQL Server}; + \
-        Servername = ejemplo123;+ \
-        Database=ejemplo123; + \
-        UID=ejemplo123; + \
-        PWD=ejemplo123')
-    print("Conexion exitosa")
-    cursor = connection.cursor()
-    cursor.execute("SELECT @@VERSION")
-    row = cursor.fetchone()
+def connection():
+    try:
+        connection = pyodbc.connect(
+            'Driver={SQL Server}; + \
+            Servername = ejemplo123;+ \
+            Database=ejemplo123; + \
+            UID=ejemplo123; + \
+            PWD=ejemplo123')
+        print("Conexion exitosa")
+        cursor = connection.cursor()
+        cursor.execute("SELECT @@VERSION")
+        row = cursor.fetchone()
 
-    cursor.execute("SELECT * FROM ejemplo123")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-except Exception as e:
-    print("Ocurrio un error al conectar a SQL Server: ", e)
+        cursor.execute("SELECT * FROM ejemplo123")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except Exception as e:
+        print("Ocurrio un error al conectar a SQL Server: ", e)
 
-finally:
-    connection.close()
-    print("Conexion cerrada")
+    finally:
+        connection.close()
+        print("Conexion cerrada")
     
+    return connection
+
+conexion = connection() 
+
+
     
